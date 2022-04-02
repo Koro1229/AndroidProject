91cb804d87a4b0f640ce48f16800ae883a545c44
Koro <youcry987951@gmail.com>
2022-04-03 03:11:05 +0900

    finish all hw6

--- 108820027_HW6_1/app/src/main/java/com/example/a108820027_hw6/MainActivity.java
+++ 108820027_HW6_1/app/src/main/java/com/example/a108820027_hw6/MainActivity.java
@@ -5,75 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean chocolate = false;
-    private boolean sprinkle = false;
-    private boolean nut = false;
-    private boolean cherry = false;
-    private boolean cookie = false;
+    private CheckBox chocolate;
+    private CheckBox sprinkle;
+    private CheckBox nut;
+    private CheckBox cherry;
+    private CheckBox cookie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chocolate = findViewById(R.id.chocolate);
+        sprinkle = findViewById(R.id.sprinkles);
+        nut = findViewById(R.id.crushed_nut);
+        cherry = findViewById(R.id.cherries);
+        cookie = findViewById(R.id.orio_cookie);
     }
 
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
-    public void onRadioButtonClicked(View view) {
-        switch (view.getId()) {
-            case R.id.chocolate:
-                if (chocolate)
-                    chocolate = false;
-                else
-                    chocolate = true;
-                break;
-            case R.id.sprinkles:
-                if (sprinkle)
-                    sprinkle = false;
-                else
-                    sprinkle = true;
-                break;
-            case R.id.crushed_nut:
-                if (nut)
-                    nut = false;
-                else
-                    nut = true;
-                break;
-            case R.id.cherries:
-                if (cherry)
-                    cherry = false;
-                else
-                    cherry = true;
-                break;
-            case R.id.orio_cookie:
-                if (cookie)
-                    cookie = false;
-                else
-                    cookie = true;
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void show_selected_toast(View view) {
+    public void onSubmit(View view) {
         String content = "";
-        if(chocolate)
+        if(chocolate.isChecked())
             content = content + getString(R.string.chocolate_syrup) + " ";
-        if(sprinkle)
+        if(sprinkle.isChecked())
             content = content + getString(R.string.sprinkles) + " ";
-        if(nut)
+        if(nut.isChecked())
             content = content + getString(R.string.crushed_nuts) + " ";
-        if(cherry)
+        if(cherry.isChecked())
             content = content + getString(R.string.cherries) + " ";
-        if(cookie)
+        if(cookie.isChecked())
             content = content + getString(R.string.orio_cookie_crumbles) + " ";
         displayToast(content);
     }
